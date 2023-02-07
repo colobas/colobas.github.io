16e54cb94d688904e2b4165fc1ce394748b57205
Guilherme Pires <guilherme.pires@alleninstitute.org>
2023-02-07 13:20:12 -0800

    comment out graph

--- svelte-components/src/main.js
+++ svelte-components/src/main.js
@@ -2,17 +2,17 @@ import BlinkingCursor from './BlinkingCursor.svelte';
 import TypeTitle from './TypeTitle.svelte';
 import Graph from './Graph.svelte';
 
-fetch("/graph.json")
-  //.then(response => response.json())
-  .then(async function(response){ response = await response;  return response.json(); })
-  .then(data =>
-    new Graph({
-      target: document.querySelector('#graph-holder'),
-      props: {
-      	graph: data
-      }
-    }),
-  );
+//fetch("/graph.json")
+//  //.then(response => response.json())
+//  .then(async function(response){ response = await response;  return response.json(); })
+//  .then(data =>
+//    new Graph({
+//      target: document.querySelector('#graph-holder'),
+//      props: {
+//      	graph: data
+//      }
+//    }),
+//  );
 
 new TypeTitle({
   target: document.querySelector('#hello-friend'),
